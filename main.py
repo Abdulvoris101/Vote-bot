9b9919cff353f41ea5242cd9da3e0331ed475b13
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2022-09-01 15:43:24 +0500

    Complete bot

--- main.py
+++ main.py
@@ -1,53 +1,50 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
+from main import vote_to
 import telebot
+from telebot import types
 
 token = '5664019486:AAGdBQKWdic-MVhcZtx11h6wAMGwXnEE900'
 
 
-bot = telebot.TeleBot(token, parse_mode='HTML') 
+bot = telebot.TeleBot(token, parse_mode='HTML')
 
-options = webdriver.ChromeOptions()
-options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36')
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+	bot.send_message(message.chat.id, f"Assalomu aleykum <b>{message.from_user.first_name}</b> botimizga xush kelibsiz.\nOvoz berish uchun /vote buyru'gini yuboring")
 
-driver = webdriver.Chrome(executable_path='C:\\Users\\User\\Desktop\\Code\\py_bots\\vote_bot\\chromedriver.exe', options=options)
-url = "https://openbudget.uz/boards/6/129485/"
+@bot.message_handler(commands=['vote'])
+def send_welcome(message):
+	# msg = bot.reply_to(message, f"Ovoz berishdan avval telefon nomeringizni yuboring  Masalan: 909175555")
+    msg = bot.reply_to(message, f"Ovoz berishdan avval telefon nomeringizni yuboring\nMasalan: <b>909175555</b>,\nIltimos <b>+998</b> ishlatmang!")
+    bot.register_next_step_handler(msg, user_answer)
 
-try:
-    driver.get(url=url)
 
-    vote_btn = driver.find_element(By.XPATH, '//*[@id="__layout"]/div/section/div/div/div[2]/div/div[5]/div[2]/div/a')
-    vote_btn.click()
 
-    time.sleep(2)
+def user_answer(msg):
+    msg_str = str(msg.text)
+    if msg_str[0] == '+':
+        msg = bot.send_message(msg.chat.id, "Iltimos to'gri nomer kiriting")
+        bot.register_next_step_handler(msg, user_answer)
+    elif len(msg_str) <  9 or len(msg_str) >= 10:
+        bot.send_message(msg.chat.id, 'Iltimos uzb nomer kiriting')
+        bot.register_next_step_handler(msg, user_answer)
+    else:
+        kb = types.InlineKeyboardMarkup()
+        item_yes = types.InlineKeyboardButton(text='Xa', callback_data='yes')
+        item_no = types.InlineKeyboardButton(text='Yoq', callback_data='no')
+        kb.add(item_yes, item_no)
 
-    get_sms = driver.find_element(By.XPATH, '//*[@id="__layout"]/div/section/div[2]/div/div[2]/div/a')
-    get_sms.click()
+        global phone_num
+        phone_num = msg_str
 
-    time.sleep(2)
+        msg = bot.send_message(msg.chat.id, 'Ovoz berish uchun <b>Xa</b> ni bosing', reply_markup=kb)
 
-    phone_num = driver.find_element(By.ID, 'phone')
-    phone_num.send_keys("909174227")
-    
-    time.sleep(3)
-    
+@bot.callback_query_handler(func= lambda call:True)
+def answer(call):
+    if call.data == 'yes':
+        vote_to(phone_num)
+        bot.send_message(call.message.chat.id, 'Ovoz berganingiz uchun raxmat')
+    elif call.data == 'no':
+        bot.send_message(call.message.chat.id, 'Ovoz berish bekor qilindi')
 
-    verif_phone = driver.find_element(By.XPATH, '//*[@id="__layout"]/div/section/div[2]/div/div[2]/form/div[2]/button')
-    verif_phone.click()
 
-    time.sleep(3)
-
-    key = str(input('Key: '))
-    time.sleep(2)
-
-    input_verif = driver.find_element(By.XPATH, '//*[@id="phone"]').send_keys(key)
-    time.sleep(2)
-    driver.find_element(By.XPATH, '//*[@id="__layout"]/div/section/div[2]/div/div[2]/form/div[2]/button').click()
-
-    time.sleep(5)
-
-except Exception as ex:
-    print(ex)
-finally:
-    driver.close()
+bot.polling()
