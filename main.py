b722bd841a6f939d7eb9d3c50e123b33ec40f1c9
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2022-09-02 19:51:15 +0500

    Finish

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
+from tokenize import Token
 import telebot
 from telebot import types
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import time
 import random
-from fake_useragent import UserAgent
+from selenium_py.sl import Vote
+from Config.config import TOKEN
 
-
-token = '5664019486:AAGdBQKWdic-MVhcZtx11h6wAMGwXnEE900'
+token = TOKEN # your token
 
 
 bot = telebot.TeleBot(token, parse_mode='HTML')
-
-useragent = UserAgent()
-options = webdriver.ChromeOptions()
-options.add_argument(f'user-agent={useragent.random}')
 
 
 
@@ -23,13 +18,9 @@ options.add_argument(f'user-agent={useragent.random}')
 def send_welcome(message):
     bot.send_message(message.chat.id, f"Assalomu aleykum <b>{message.from_user.first_name}</b> botimizga xush kelibsiz.\nOvoz berish uchun /vote buyru'gini yuboring")
 
-
-
 @bot.message_handler(commands=['vote'])
 def send_welcome(message):
-    
-# msg = bot.reply_to(message, f"Ovoz berishdan avval telefon nomeringizni yuboring  Masalan: 909175555")
-    msg = bot.reply_to(message, f"Ovoz berishdan avval telefon nomeringizni yuboring\nMasalan: <b>909175555</b>,\nIltimos <b>+998</b> ishlatmang!")
+    msg = bot.reply_to(message, f"Ovoz berish uchun telefon nomeringizni yuboring\nMasalan: <b>909175555</b>,\nIltimos <b>+998</b> ishlatmang!")
     bot.register_next_step_handler(msg, user_answer)
 
 def user_answer(msg):
@@ -44,69 +35,8 @@ def user_answer(msg):
     else:
         
         phone_numb = msg_str
-        # Selenium 
-
         
-
-        global driver
-        driver = webdriver.Chrome(executable_path='C:\\Users\\User\\Desktop\\Code\\py_bots\\vote_bot\\chromedriver.exe', options=options)
-        url = "https://openbudget.uz/boards/6/129485/"
-        # url = 'https://www.whatismybrowser.com/detect/what-is-my-user-agent/'
-
-        driver.get(url=url)
-
-        vote_btn = driver.find_element(By.XPATH, '//*[@id="__layout"]/div/section/div/div/div[2]/div/div[5]/div[2]/div/a')
-        vote_btn.click()
-
-        time.sleep(2)
-
-        get_sms = driver.find_element(By.XPATH, '//*[@id="__layout"]/div/section/div[2]/div/div[2]/div/a')
-        get_sms.click()
-
-        time.sleep(3)
-    
-        phone_num = driver.find_element(By.ID, 'phone')
-        phone_num.send_keys(phone_numb)
-
-        time.sleep(5)
-
-
-        verif_phone = driver.find_element(By.XPATH, '//*[@id="__layout"]/div/section/div[2]/div/div[2]/form/div[2]/button')
-        verif_phone.click()
-        
-
-        form = driver.find_element(By.CLASS_NAME, 'form')
-        time.sleep(6)
-        
-        error = form.find_element(By.XPATH, '//*[@id="__layout"]/div/section/div[2]/div/div[2]/form/p')
-        
-        if error:
-            bot.send_message(msg.chat.id, 'Siz ovoz bergansiz!')
-            driver.close()
-        
-        time.sleep(5)
-
-
-        key = bot.send_message(msg.chat.id, 'Sms kodni kiriting')
-
-        bot.register_next_step_handler(key, sms_verif)
-
-
-def sms_verif(key):
-    input_verif = driver.find_element(By.XPATH, '//*[@id="phone"]').send_keys(key)
-
-    time.sleep(2)
-
-    driver.find_element(By.XPATH, '//*[@id="__layout"]/div/section/div[2]/div/div[2]/form/div[2]/button').click()
-
-    time.sleep(5)
-
-    driver.close()
-
-
-
-
-
+        Vote(phone_number=phone_numb, bot=bot, msg=msg)
 
 
 bot.polling()
---
+++ selenium_py/sl.py
@@ -0,0 +1,179 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from fake_useragent import UserAgent
+from selenium.common.exceptions import NoSuchElementException
+from bs4 import BeautifulSoup
+import requests 
+from tqdm import tqdm
+import sqlite3
+from datetime import datetime
+
+db = sqlite3.connect('server.db', check_same_thread=False)
+sql = db.cursor()
+
+
+
+url = "https://openbudget.uz/boards/6/156480"
+useragent = UserAgent()
+options = webdriver.ChromeOptions()
+options.add_argument(f'user-agent={useragent.random}')
+options.headless = True
+options.add_argument("--window-size=1920,1080")
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--allow-running-insecure-content')
+options.add_argument("--disable-extensions")
+options.add_argument("--proxy-server='direct://'")
+options.add_argument("--proxy-bypass-list=*")
+options.add_argument("--start-maximized")
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--no-sandbox')
+
+sql.execute("""CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name VARCHAR(250),
+    username VARCHAR(250),
+    telegram_id BIGINT,
+    status BOOLEAN,
+    phone_number TEXT,
+    joined DATETIME
+)""")
+
+db.commit()
+
+
+class Vote:
+    def __init__(self, phone_number, bot, msg):
+
+        telegram_id = msg.chat.id
+        first_name = msg.chat.first_name
+        username = msg.chat.username
+        status = 0
+        joined = datetime.now()
+
+        values = first_name, username, telegram_id, status, phone_number, joined
+
+        # sql.execute("""SELECT * from users""")
+        
+        self.phone_number = phone_number
+        sql.execute("SELECT id from users")
+
+        if sql.fetchone() is None:
+            sql.execute(f"INSERT INTO users(first_name, username, telegram_id, status, phone_number, joined) VALUES(?, ?, ?, ?, ?, ?)", values) 
+            db.commit()
+        else:
+            print('Have')
+
+        driver = webdriver.Chrome(executable_path='C:\\Users\\User\\Desktop\\Code\\py_bots\\vote_bot\\chromedriver.exe', options=options)
+
+        self.driver = driver
+        self.bot = bot
+
+        driver.get(url=url)
+        
+        html = driver.page_source
+
+        self.server_m = bot.send_message(msg.chat.id, 'Serverdan javob kutilmoqda...')
+
+        if self.parsing(html):
+            
+            # Assaign values to total and current values
+            self.main(bot, msg)
+
+        else:
+            time.sleep(5)
+            driver.refresh()
+            time.sleep(10)
+            self.main(bot, msg)
+            bot.send_message(msg.chat.id, 'Server javob bermayapti, Iltimos biroz kuting')
+
+    
+    def main(self, bot, msg):
+        # vote_btn click
+        self.driver.find_element(By.XPATH, '//*[@id="__layout"]/div/section/div/div/div[2]/div/div[5]/div[2]/div/a').click()
+
+        time.sleep(2)
+
+        # click to sms verification
+
+        self.driver.find_element(By.XPATH, '//*[@id="__layout"]/div/section/div[2]/div/div[2]/div/a').click()
+
+        bot.delete_message(msg.chat.id, self.server_m.message_id)
+        one_m = bot.send_message(msg.chat.id, 'Bir daqiqa...')
+
+        time.sleep(3)
+    
+        # phone_num - input fill phone number
+
+        self.driver.find_element(By.ID, 'phone').send_keys(self.phone_number)
+
+        bot.delete_message(msg.chat.id, one_m.message_id)
+
+        tele_m = bot.send_message(msg.chat.id, 'Telefon nomer kiritilinmoqda...')
+
+        time.sleep(5)
+
+        # Send sms to verifaction
+
+        self.driver.find_element(By.XPATH, '//*[@id="__layout"]/div/section/div[2]/div/div[2]/form/div[2]/button').click()
+    
+
+        form = self.driver.find_element(By.CLASS_NAME, 'form')
+
+        time.sleep(6)
+
+
+        try:
+            error = form.find_element(By.XPATH, '//*[@id="__layout"]/div/section/div[2]/div/div[2]/form/p')
+            
+            if error:
+                bot.delete_message(msg.chat.id, tele_m.message_id)
+                bot.send_message(msg.chat.id, 'Siz ovoz bergansiz /start')
+                time.sleep(5)
+                self.driver.close()
+
+        except:
+            bot.delete_message(msg.chat.id, tele_m.message_id)
+            msg_key = bot.send_message(msg.chat.id, 'Sms kodni kiriting')
+            bot.register_next_step_handler(msg_key, self.sms_verif)
+
+
+
+    def sms_verif(self, msg_key):
+        input_verif = self.driver.find_element(By.XPATH, '//*[@id="phone"]').send_keys(msg_key.text)
+
+        time.sleep(2)
+
+        self.driver.find_element(By.XPATH, '//*[@id="__layout"]/div/section/div[2]/div/div[2]/form/div[2]/button').click()
+
+        checked_m = self.bot.send_message(msg_key.chat.id, "Tekshirilmoqda...")
+
+        time.sleep(5)
+
+        try:
+            error = self.driver.find_element(By.XPATH, '//*[@id="__layout"]/div/section/div[2]/div/div[2]/form/p')
+
+            if error:
+                self.bot.delete_message(msg_key.chat.id, checked_m.message_id)
+                self.bot.send_message(msg_key.chat.id, "Notog'ri kod! Boshqatan urinib koring \n /start")
+                self.driver.close()
+        except:
+            self.bot.delete_message(msg_key.chat.id, checked_m.message_id)
+            self.bot.send_message(msg_key.chat.id, "Ovoz berganingiz uchun raxmat")
+
+            
+
+            self.driver.close()
+            
+    
+    def parsing(self, html):
+        soup = BeautifulSoup(html, 'lxml')
+        txt = soup.find('div', class_='pages-title')
+        
+        if txt:
+            return True
+
+        return False
+        
+    
