87feb5822511081e37ed6239b034814e2f8e1e82
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2022-09-08 16:49:14 +0500

    Finishing

--- selenium_py/sl.py
+++ selenium_py/sl.py
@@ -28,18 +28,13 @@ options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-
-
 options.add_argument("--window-size=1920,1080")
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--allow-running-insecure-content')
 options.add_argument("--disable-extensions")
-options.add_argument("--proxy-server='direct://'")
-options.add_argument("--proxy-bypass-list=*")
-options.add_argument("--start-maximized")
+# options.add_argument("--proxy-server='direct://'")
+# options.add_argument("--proxy-bypass-list=*")
+# options.add_argument("--start-maximized")
 options.add_argument('--disable-gpu')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--no-sandbox')
@@ -83,8 +78,7 @@ class Vote:
     def __init__(self, bot, msg):
         telegram_id = msg.chat.id
         phone_number = sql.execute(f"SELECT phone_number FROM users WHERE telegram_id={telegram_id}").fetchone()
-        
-        
+
 
         driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
